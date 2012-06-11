ed792375b626454b8a0f028a254a5226d0784e20
Rajib Chowdhury <chowdhury284@gmail.com>
2012-06-12 00:55:20 +0530

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jenkintest::Application.config.secret_token = '9bea2aaa6be490db2f1639958658045646523b15306a00c5756fc0883cfe4fa5fef157465bc3a67596deb438ac7e9e025acf8a8e9ef7a27f53a3a6dd493a4805'
